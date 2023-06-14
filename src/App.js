e6794d4501d8237a666cf80382b726f4fb1ca71c
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-06-14 12:31:40 -0700

    Building App file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import AngryButton from './components/fancy-buttons/AngryButton';
+import CounterButton from './components/fancy-buttons/CounterButton';
+import LightSwitchButton from './components/fancy-buttons/LightSwitchButton';
+import TextRepeaterButton from './components/fancy-buttons/TextRepeaterButton';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={`App`}>
+      <h1>Fancy Buttons!</h1>
+      <section>
+        <AngryButton />
+        <CounterButton />
+        <LightSwitchButton />
+        <TextRepeaterButton />
+      </section>
     </div>
   );
 }
